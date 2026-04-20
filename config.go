b3852d5d58a7d4bb583452b777d9af609c7e0044
Abdullah <158028266+Almutairi0@users.noreply.github.com>
2026-04-20 11:04:06 +0300

    Update Config struct to add WatchDirection and DestinationDirection fields

---
+++ config.go
@@ -0,0 +1,6 @@
+// Config struct contains configuration settings for relayfsd.
+type Config struct {
+    // Existing fields...
+    WatchDirection  string // Direction to watch
+    DestinationDirection string // Direction for destination
+}
