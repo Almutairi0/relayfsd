6ef841e58dbc4ee410c5213c22e1582fdda034c2
Almutairi0 <Almutairi.swe@gmail.com>
2026-04-23 18:03:09 +0300

    did that one by mistake

--- config.go
+++
@@ -1,6 +0,0 @@
-// Config struct contains configuration settings for relayfsd.
-type Config struct {
-    // Existing fields...
-    WatchDirection  string // Direction to watch
-    DestinationDirection string // Direction for destination
-}
